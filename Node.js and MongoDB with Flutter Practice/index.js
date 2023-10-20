da7a3097ead3350da15e5be18926c336a144201f
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-10-19 22:39:10 -0700

    post method is ok

--- Node.js and MongoDB with Flutter Practice/index.js
+++ Node.js and MongoDB with Flutter Practice/index.js
@@ -2,8 +2,36 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT | 3000;
 
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
+const productData = [];
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+// post api
+app.post('/api/add_product', (req, res) => {
+
+    console.log('Result', req.body);
+
+    const pdata = {
+        "id": productData.length + 1,   // will be auto incremented when new data is added by one
+        "pname": req.body.pname,
+        "pprice": req.body.pprice,
+        "pdescription": req.body.pdescription
+    };
+
+    productData.push(pdata);
+    console.log('Final', pdata);
+
+    res.status(200).send({
+        "status_code": 200,
+        "message": "Product added successfully",
+        "product": pdata
+    });
 });
