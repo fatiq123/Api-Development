416032c624182d96bff6356f9cf500e71800a242
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-10-19 21:15:36 -0700

    change db name in mongodb

--- Node.js and MongoDB Api/utils/db.js
+++ Node.js and MongoDB Api/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set('strictQuery', true);
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/lab6", {
+mongoose.connect("mongodb://127.0.0.1:27017/productDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
