1630720472934cabf5e2533b011cd63b898219ba
FATIQ HUSSNAIN <fatiqhussnain1@gmail.com>
2023-10-24 05:36:50 -0700

    added body-parser

--- Node.js and MongoDB with Flutter Practice/index.js
+++ Node.js and MongoDB with Flutter Practice/index.js
@@ -3,9 +3,13 @@ const app = express();
 const PORT = process.env.PORT | 3000;
 const mongoose = require('mongoose');
 
-app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const body_parser = require('body-parser');
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json());
+
+// app.use(express.urlencoded({ extended: true }));
 
 const productData = [];
 
